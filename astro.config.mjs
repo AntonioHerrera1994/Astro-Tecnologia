a0d97e26829bc154f3c2b1f2850eda8cbdf04207
Antonio Herrera <herreraquinteroj@gmail.com>
2025-01-10 15:33:13 -0800

    Quite node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
