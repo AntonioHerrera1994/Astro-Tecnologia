8a9b41847d79a72fe47b4813dc1124b529978673
Antonio Herrera <herreraquinteroj@gmail.com>
2025-04-28 19:30:19 -0700

    faq

--- src/scripts/acordeon.js
+++ src/scripts/acordeon.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const accordionButtons = document.querySelectorAll('.accordion-button');
-  const accordionContents = document.querySelectorAll('.accordion-content');
+const accordionButtons = document.querySelectorAll('.accordion-button');
+const accordionContents = document.querySelectorAll('.accordion-content');
 
-  if (accordionButtons.length > 0) {
-    accordionButtons[0].parentNode.classList.add('active');
-    accordionContents[0].style.maxHeight = accordionContents[0].scrollHeight + 'px';
+// Activar el primer elemento al cargar la página
+accordionButtons[0].parentNode.classList.add('active');
+accordionContents[0].style.maxHeight = accordionContents[0].scrollHeight + 'px';
 
-    accordionButtons.forEach((button) => {
-      button.addEventListener('click', () => {
-        const accordionItem = button.parentNode;
-        const accordionContent = accordionItem.querySelector('.accordion-content');
+accordionButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const accordionItem = button.parentNode;
+    const accordionContent = accordionItem.querySelector('.accordion-content');
 
-        if (accordionItem.classList.contains('active')) {
-          accordionItem.classList.remove('active');
-          accordionContent.style.maxHeight = '0';
-        } else {
-          accordionButtons.forEach(otherButton => {
-            otherButton.parentNode.classList.remove('active');
-            otherButton.parentNode.querySelector('.accordion-content').style.maxHeight = '0';
-          });
-
-          accordionItem.classList.add('active');
-          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-        }
+    if (accordionItem.classList.contains('active')) {
+      accordionItem.classList.remove('active');
+      accordionContent.style.maxHeight = '0'; // Cierra con desplazamiento hacia arriba
+    } else {
+      accordionButtons.forEach(otherButton => {
+        otherButton.parentNode.classList.remove('active');
+        otherButton.parentNode.querySelector('.accordion-content').style.maxHeight = '0';
       });
-    });
-  }
+
+      accordionItem.classList.add('active');
+
+      // Calcula la altura del contenido y aplica el desplazamiento
+      const contentHeight = accordionContent.scrollHeight;
+      accordionContent.style.maxHeight = contentHeight + 'px';
+    }
+  });
 });
